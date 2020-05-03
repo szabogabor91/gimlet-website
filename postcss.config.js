3bcb77e5d3e0a43f018c7f8763527532c47b7417
Laszlo Fogas <laszlo@laszlo.cloud>
2020-05-03 22:29:24 +0200

    Pointing to prismJS classes to survive purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -15,7 +15,6 @@ if (!isDev) {
   [].push.apply(plugins, [
     purgecss({
       content: ['src/**/*.njk', 'src/**/*.md', 'src/**/*.js'],
-      whitelist: ['token', 'pre', 'code'],
       defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
     }),
     cssnano({
